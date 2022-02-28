f6277c272359478164f8d159a6d18a0638a1145e
Sean Kwok <45260108+Gitshaoxiang@users.noreply.github.com>
2022-02-28 16:46:15 +0800

    update some api

--- src/M5_IP5209.cpp
+++ src/M5_IP5209.cpp
@@ -5,8 +5,7 @@ bool M5_IP5209::begin(TwoWire *wire, uint8_t sda, uint8_t scl, uint8_t addr) {
     _addr = addr;
     _sda  = sda;
     _scl  = scl;
-    _wire->begin(_sda, _scl, 100);
-    delay(10);
+    _wire->begin(_sda, _scl, 400000UL);
     _wire->beginTransmission(_addr);
     uint8_t error = _wire->endTransmission();
     if (error == 0) {
@@ -16,49 +15,91 @@ bool M5_IP5209::begin(TwoWire *wire, uint8_t sda, uint8_t scl, uint8_t addr) {
     }
 }
 
-void M5_IP5209::writeBytes(uint8_t addr, uint8_t reg, uint8_t *buffer,
+bool M5_IP5209::writeBytes(uint8_t addr, uint8_t reg, uint8_t *buffer,
                            uint8_t length) {
     _wire->beginTransmission(addr);
     _wire->write(reg);
     for (int i = 0; i < length; i++) {
         _wire->write(*(buffer + i));
     }
-    _wire->endTransmission();
+    if (_wire->endTransmission() == 0) return true;
+    return false;
 }
 
-void M5_IP5209::readBytes(uint8_t addr, uint8_t reg, uint8_t *buffer,
+bool M5_IP5209::readBytes(uint8_t addr, uint8_t reg, uint8_t *buffer,
                           uint8_t length) {
     uint8_t index = 0;
     _wire->beginTransmission(addr);
     _wire->write(reg);
     _wire->endTransmission();
-    _wire->requestFrom(addr, length);
-    for (int i = 0; i < length; i++) {
-        buffer[index++] = _wire->read();
+    if (_wire->requestFrom(addr, length)) {
+        for (int i = 0; i < length; i++) {
+            buffer[index++] = _wire->read();
+        }
+        return true;
+    }
+    return false;
+}
+
+int M5_IP5209::getBatVAdc() {
+    uint8_t data[2];
+    if (readBytes(M5_IP5209_ADDRESS, M5_IP5209_BAT_VADC_REG, data, 2)) {
+        uint16_t voltage_h = data[1] & 0x3f;
+        return (voltage_h << 8) | data[0];
+    }
+    return -1;
+}
+
+int M5_IP5209::getBatIAdc() {
+    uint8_t data[2];
+    if (readBytes(M5_IP5209_ADDRESS, M5_IP5209_BAT_IADC_REG, data, 2)) {
+        if ((data[1] & 0x20) == 0x20) {
+            charged       = false;
+            uint8_t temp1 = ~data[1];
+            uint8_t temp2 = (~(data[1] & 0x1f) & 0x1f);
+            return (temp2 * 256 + temp1 + 1);
+        } else {
+            charged            = true;
+            uint16_t current_h = data[1] & 0xff;
+            return ((current_h << 8) | data[0]);
+        }
+    }
+    return -1;
+}
+
+int M5_IP5209::getBatOCV() {
+    uint8_t data[2];
+    if (readBytes(M5_IP5209_ADDRESS, M5_IP5209_BAT_OCV_REG, data, 2)) {
+        uint16_t voltage_h = data[1] & 0x3f;
+        return (voltage_h << 8) | data[0];
+    }
+    return -1;
+}
+
+bool M5_IP5209::getBatVoltage() {
+    uint16_t _bat_voltage_adc = getBatVAdc();
+    if (_bat_voltage_adc != -1) {
+        voltage = _bat_voltage_adc * 0.00026855 + 2.6;
+        return true;
+    }
+    return false;
+}
+
+bool M5_IP5209::getBatCurrent() {
+    uint16_t _bat_current_adc = getBatIAdc();
+    if (_bat_current_adc != -1) {
+        current = _bat_current_adc * 0.745985;
+        return true;
+    } else {
+        return false;
     }
 }
 
-uint16_t M5_IP5209::getBatVoltageAdc() {
-    uint8_t data[2];
-    readBytes(M5_IP5209_ADDRESS, M5_IP5209_BAT_VOLTAGE_REG, data, 2);
-    uint16_t voltage_h = data[1];
-    return (voltage_h << 8) | data[0];
-}
-
-uint16_t M5_IP5209::getBatCurrentAdc() {
-    uint8_t data[2];
-    readBytes(M5_IP5209_ADDRESS, M5_IP5209_BAT_VOLTAGE_REG, data, 2);
-    uint16_t cureent_h = data[1];
-    return (cureent_h << 8) | data[0];
-}
-
-float M5_IP5209::getBatVoltage() {
-    uint16_t _bat_voltage_adc = getBatVoltageAdc();
-    return (float)_bat_voltage_adc * 0.00026855 + 2.6;
-    ;
-}
-
-float M5_IP5209::getBatCurrent() {
-    uint16_t _bat_current_adc = getBatCurrentAdc();
-    return (float)_bat_current_adc * 0.745985;
+bool M5_IP5209::update() {
+    if (getBatCurrent() && getBatVoltage()) {
+        power = voltage * current / 1000;
+        return true;
+    } else {
+        return false;
+    }
 }
--- src/M5_IP5209.h
+++ src/M5_IP5209.h
@@ -4,9 +4,11 @@
 #include <Arduino.h>
 #include <Wire.h>
 
-#define M5_IP5209_ADDRESS         0x75
-#define M5_IP5209_BAT_VOLTAGE_REG 0xA2
-#define M5_IP5209_BAT_CURRENT_REG 0XA4
+#define M5_IP5209_ADDRESS      0x75
+#define M5_IP5209_BAT_VADC_REG 0xa2
+#define M5_IP5209_BAT_IADC_REG 0xa4
+#define M5_IP5209_BAT_OCV_REG  0xa8
+#define M5_IP5209_READ0_REG    0x70
 
 class M5_IP5209 {
    private:
@@ -14,16 +16,22 @@ class M5_IP5209 {
     TwoWire *_wire;
     uint8_t _sda;
     uint8_t _scl;
-    void writeBytes(uint8_t addr, uint8_t reg, uint8_t *buffer, uint8_t length);
-    void readBytes(uint8_t addr, uint8_t reg, uint8_t *buffer, uint8_t length);
+    bool writeBytes(uint8_t addr, uint8_t reg, uint8_t *buffer, uint8_t length);
+    bool readBytes(uint8_t addr, uint8_t reg, uint8_t *buffer, uint8_t length);
 
    public:
     bool begin(TwoWire *wire = &Wire, uint8_t sda = 0, uint8_t scl = 26,
                uint8_t addr = M5_IP5209_ADDRESS);
-    uint16_t getBatVoltageAdc();
-    uint16_t getBatCurrentAdc();
-    float getBatVoltage();
-    float getBatCurrent();
+    int getBatVAdc();
+    int getBatIAdc();
+    int getBatOCV();
+    bool getBatVoltage();
+    bool getBatCurrent();
+    bool update();
+    float voltage = 0;
+    float current = 0;
+    float power = 0;
+    bool charged  = false;
 };
 
 #endif
